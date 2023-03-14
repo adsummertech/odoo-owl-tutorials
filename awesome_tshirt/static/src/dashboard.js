2cdd71cf49a4efa4a3c3cbaebe0814da2bbcd920
AD SummerTech <adonato@summertech.com.au>
2023-03-14 16:32:04 +1100

    Update

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,14 @@
 /** @odoo-module **/
 
-import { registry } from "@web/core/registry";
+import { registry } from "@web/core/registry"
+import { Layout } from "@web/search/layout"
+import { useService } from "@web/core/utils/hooks";
 
 const { Component } = owl;
 
 class AwesomeDashboard extends Component {
     static template = "awesome_tshirt.dashboard"
+    static components = { Layout }
 }
 
 AwesomeDashboard.components = {};
--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 ADDONS="awesome_gallery,awesome_tshirt,owl_playground"
 DBNAME="tutorialsdb"
-/mnt/d/dev/odoo/odoo-bin --addons-path="/home/ad/dev/odoo/addons/,." -d "$DBNAME" -i "$ADDONS" -u "$ADDONS" --dev xml --workers 0
+odoo --addons-path=",." -d "$DBNAME" -i "$ADDONS" -u "$ADDONS" --dev xml --workers 0
 
 #reload
 
